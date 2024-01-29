67df1147a2c89cf9ad15d7a5c8d5f20582e3d5dc
Jonas Tüchler <jonas.tuechler@gmail.com>
2024-01-29 17:14:48 +0100

    time loggin for influxdb streamlit

--- streamlit_app_influxdb.py
+++ streamlit_app_influxdb.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from influxdb import DataFrameClient
+import time
 
 # InfluxDB connection parameters
 INFLUXDB_HOST = 'localhost'
@@ -13,23 +14,30 @@ INFLUXDB_DBNAME = 'awattardb'
 
 # Establish a connection to the InfluxDB database
 def connect_to_influxdb():
-    return DataFrameClient(
+    start_time = time.time()
+    client = DataFrameClient(
         host=INFLUXDB_HOST, 
         port=INFLUXDB_PORT, 
         username=INFLUXDB_USERNAME, 
         password=INFLUXDB_PASSWORD, 
         database=INFLUXDB_DBNAME
     )
+    end_time = time.time()
+    print(f"Time taken to connect to InfluxDB Database: {end_time - start_time} seconds")
+    return client
 
 # Load data from InfluxDB database
 @st.cache_data
 def load_data_from_influxdb():
+    start_time = time.time()
     client = connect_to_influxdb()
     # Query the measurement (table) where your data is stored
     query = 'SELECT * FROM "prices"'
     data = client.query(query)
     df = data['prices']  # The measurement name is the key in the returned dict
     df.index = pd.to_datetime(df.index)  # Convert the index to datetime
+    end_time = time.time()
+    print(f"Time taken to load data from InfluxDB: {end_time - start_time} seconds")
     return df
 
 # Fetch data
